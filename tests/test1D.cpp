672193d56b8b63d4c4c7c2b98a061c880323502c
pederson <dpederson@utexas.edu>
2017-07-06 18:53:32 -0500

    (added) NumericalFlux struct and implemented Lax-Friedrichs with ity

--- tests/test1D.cpp
+++ tests/test1D.cpp
@@ -24,46 +24,67 @@ struct SomeQuantity{
 };
 
 
-template <std::size_t ndim, 
-		  typename CellT,
-		  typename SolutionIterator,
-		  class ConservedQuantityFunctor,
-		  class FaceFluxFunctor,
-		  class SourceFunctors...>
+template <class ConservedQuantityFunctor,
+		  class NumericalFluxFunctor,
+		  class... SourceFunctors>
 struct ExplicitUpdate{
-	// SolutionIterator msit;
 	double mdt, mdx, mdtdx;
-	std::tuple<ConservedQuantityFunctor, FaceFluxFunctor, SourceFunctors...> mtpl;
+	std::tuple<ConservedQuantityFunctor, NumericalFluxFunctor, SourceFunctors...> mtpl;
 
-	constexpr ExplicitUpdate(double dt, double dx, std::tuple<ConservedQuantityFunctor, FaceFluxFunctor, SourceFunctors...> tpl)
+	constexpr ExplicitUpdate(double dt, double dx, std::tuple<ConservedQuantityFunctor, NumericalFluxFunctor, SourceFunctors...> tpl)
 	: mdt(dt)
 	, mdx(dx)
 	, mdtdx(dt/dx)
 	, mtpl(tpl){};
 
-	double operator()(CellT & cl){
+	template <typename CellT>
+	double operator()(const CellT & cl){
 		double flx=0.0;
-		for (auto d=0; d<ndim; d++){
-			flx += 0.5*(std::get<1>(mtpl).operator()(cl.getNeighborMin<d>()));
-			flx -= 0.5*(std::get<1>(mtpl).operator()(cl.getNeighborMax<d>()));
-		}
+
+		flx -= std::get<1>(mtpl).operator()(cl, cl.getNeighborMin(0));
+		flx += std::get<1>(mtpl).operator()(cl.getNeighborMax(0), cl);
+
 		return std::get<0>(mtpl).operator()(cl) - mdtdx*flx;
 	}
 };
 
 
-template <std::size_t ndim,
-		  typename CellT,
-		  typename SolutionIterator,
-		  class ConservedQuantityFunctor,
-		  class FaceFluxFunctor,
-		  class SourceFunctors...>
-ExplicitUpdate<ndim,CellT,SolutionIterator,
-			   ConservedQuantityFunctor,
-			   FaceFluxFunctor,
-			   SourceFunctors...> make_explicit_update(double dt, double dx, ConservedQuantityFunctor cf, FaceFluxFunctor ff, SourceFunctors... sf){
-	return ExplicitUpdate(dt, dx, std::make_tuple(cf,ff,sf));
+template <class ConservedQuantityFunctor,
+		  class NumericalFluxFunctor,
+		  class... SourceFunctors>
+auto make_explicit_update(double dt, double dx, ConservedQuantityFunctor cf, NumericalFluxFunctor ff, SourceFunctors... sf){
+	return ExplicitUpdate<ConservedQuantityFunctor,
+			   			  NumericalFluxFunctor,
+			   			  SourceFunctors...>(dt, dx, std::make_tuple(cf,ff,sf...));
 };
+
+
+template <class ConservedQuantityFunctor, 
+		  class FluxFunctor>
+struct NumericalFlux{
+	ConservedQuantityFunctor mcf;
+	FluxFunctor mff;
+	double mCc; 	// coefficient of the flux on the conserved quantity value
+	double mCn;		// coefficient of the flux on the neighbor conserved quantity value
+	double mFc;		// coefficient of the flux on the flux value
+	double mFn;		// coefficient of the flux on the neighbor flux value
+
+	NumericalFlux(ConservedQuantityFunctor cf, FluxFunctor ff, double Cc, double Cn, double Fc, double Fn)
+	: mcf(cf), mff(ff)
+	, mCc(Cc), mCn(Cn)
+	, mFc(Fc), mFn(Fn) {};
+
+	template <typename CellT>
+	double operator()(const CellT & cl, const CellT & neighb){
+		return mCc*mcf.operator()(cl)+mCn*mcf.operator()(neighb)+mFc*mff.operator()(cl)+mFn*mff.operator()(neighb);
+	}
+};
+
+template <class ConservedQuantityFunctor,
+		  class FluxFunctor>
+auto make_numerical_flux(ConservedQuantityFunctor cf, FluxFunctor ff, double Cc, double Cn, double Fc, double Fn){
+	return NumericalFlux<ConservedQuantityFunctor, FluxFunctor>(cf, ff, Cc, Cn, Fc, Fn);
+}
 
 
 // create an update struct
@@ -73,7 +94,7 @@ template <typename CellIterator,
 void for_each_update(CellIterator first, CellIterator last, Functor f, SolutionIterator sfirst){
 	auto soln = sfirst;
 	for (auto it=first; it!=last; it++){
-		(*soln) = f.operator()(it);
+		(*soln) = f.operator()(*it);
 		soln++;
 	}
 }
@@ -85,9 +106,9 @@ int main(int argc, char * argv[]){
 
 	// time-stepping parameters
 	const double c 	= 1.0;								// velocity [m/s]
-	const double cfl = 0.9;								// cfl number
+	const double cfl = 0.5;								// cfl number
 	const double dx = 1.0/static_cast<double>(ncells);	// cell size [m]
-	const double dt = cfl*dx/c;							// time step [s]
+	const double dt = cfl*dx/std::fabs(c);							// time step [s]
 
 	// std::cout << typeid(y).name() << std::endl;
 
@@ -95,31 +116,32 @@ int main(int argc, char * argv[]){
 	typedef fidi::ObjectStencil<1,1,SomeQuantity> 		CellT;
 
 	// define the domain data structure and connect neighbors
-	std::vector<CellT> cells(50);
-	for (auto i=1; i<49; i++){
-		cells[i].setNeighborMin(0, cells[i-1]);
-		cells[i].setNeighborMax(0, cells[i+1]);
+	std::vector<CellT> cells(ncells);
+	for (auto i=1; i<ncells-1; i++){
+		cells[i].setNeighborMin(cells[i-1], 0);
+		cells[i].setNeighborMax(cells[i+1], 0);
 	}
 
 	// define the initial density
-	for (auto i=20; i<30; i++) cells[i].n() = 1.0;
-
-	// define the conserved quantities 
-	// std::tuple<std::function<const double(CellT &)>, std::function<const double(CellT &)>> 	conserved = {, };
+	for (auto i=20; i<30; i++) cells[i].n() = 1.0-std::fabs(i-25)/5.0;
 
 	// initialize a solution vector
 	std::vector<double> soln(ncells, 0.0);
 
 	// make the explicit update struct
-	auto exp_up = make_explicit_update(dt, dx, [](CellT & cl){return cl.n();}, [&c](CellT & cl){return cl.n()*c;});
+	auto cons = [](const CellT & cl){return cl.n();};
+	auto flux = [&c](const CellT & cl){return c*cl.n();};
+	auto lax_fried = make_numerical_flux(cons, flux, -0.5*dx/dt, 0.5*dx/dt, 0.5, 0.5);
+	auto exp_up = make_explicit_update(dt, dx, cons, lax_fried);
 	
 	// start time-stepping
-	for (auto t=0; t<200; t++){
+	for (auto t=0; t<20; t++){
 		for_each_update(++cells.begin(), --cells.end(), exp_up, ++soln.begin());
-		for_each_update(++soln.begin(), --soln.end(), [](double & d){}, ++cells.begin());
+		for(auto i=0; i<ncells; i++) cells[i].n() = soln[i];
+		// for_each_update(++soln.begin(), --soln.end(), [](double & d){}, ++cells.begin());
 
 		// std::cout << "n:" ;
-		for(auto i=0; i<50; i++) std::cout << ", " << cells[i].n() ;
+		for(auto i=0; i<ncells; i++) std::cout << ", " << cells[i].n() ;
 		std::cout << std::endl;
 	}
 
